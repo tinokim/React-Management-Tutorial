6240faf90bdb2aa8da92648405b176af192c0cbd
tinokim <gksmfwlsgh@nate.com>
2020-10-23 15:19:41 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './componunts/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let'ss develop management system!</h2>
+const customers = [{
+  'id' : 1,
+  'image':'https://placeimg.com/64/64/1',
+  'name' : '김진호',
+  'gender' : '남자',
+  'job' : '회사원'
+},
+{
+  'id' : 2,
+  'image':'https://placeimg.com/64/64/2',
+  'name' : '한상희',
+  'gender' : '남자',
+  'job' : '개발자'
+},
+{
+  'id' : 3,
+  'image':'https://placeimg.com/64/64/3',
+  'name' : '김수경',
+  'gender' : '여자',
+  'job' : '개발자'
+}
+]
+class App extends Component{
+  render(){
+    return(
+      <div>
+    {
+      customers.map(c=>{
+        return(
+          <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job} 
+          />
+        )
+      })
+    }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
